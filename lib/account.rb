846fcb152382dcaf2c093c037cd4613b97ba50bb
Sabrina <sabrinalord@msn.com>
2022-01-31 14:27:59 +0000

    added transaction history to account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,9 @@
 class Account 
-  attr_reader :balance
+  attr_reader :balance, :transaction_history
 
   def initialize
     @balance = 0
+    @transaction_history = []
 
   end 
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,6 +6,9 @@ describe Account do
     it 'has a balance of 0' do
       expect(subject.balance).to eq(0)
     end
-    
+
+    it 'begins with an empty transaction history' do
+      expect(subject.transaction_history).to eq([])
+    end     
   end
 end
