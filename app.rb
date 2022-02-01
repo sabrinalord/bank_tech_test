fb7eb4ce777d6689181ee28847717fbd16482651
Sabrina <sabrinalord@msn.com>
2022-02-01 17:05:58 +0000

    added app.rb file as stand in controller

---
+++ app.rb
@@ -0,0 +1,14 @@
+require './lib/account'
+require './lib/deposit'
+require './lib/statement'
+require './lib/withdraw'
+
+
+# Uncomment the below or paste into IRB
+# account = Account.new
+# account.execute_command(Deposit.new(20))
+# account.execute_command(Deposit.new(40))
+# account.execute_command(Deposit.new(20))
+# account.execute_command(Withdraw.new(10))
+# statement = Statement.new(account)
+# statement.print_out
