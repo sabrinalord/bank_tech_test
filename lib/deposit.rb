6c65ee3f2b85860edabedc3cfac900aa487ba0cf
Sabrina <sabrinalord@msn.com>
2022-02-01 18:52:54 +0000

    removed commented out code

--- lib/deposit.rb
+++ lib/deposit.rb
@@ -2,9 +2,6 @@ class Deposit
   attr_reader :credit, :date, :debit, :new_balance
 
   def initialize(amount_to_deposit)
-    # @amount_to_deposit = amount_to_deposit
-    # @transaction_date = Time.now.strftime("%d/%m/%Y")
-    # @balance_after_transaction = 0
     @credit = amount_to_deposit
     @debit = 0
     @date = Time.now.strftime("%d/%m/%Y")
