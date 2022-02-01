9520bd49296aebc3e989200b9bc964dab749c835
Sabrina <sabrinalord@msn.com>
2022-02-01 19:01:54 +0000

    commented out irb commands

--- app.rb
+++ app.rb
@@ -6,10 +6,10 @@ require './lib/statement'
 require './lib/withdraw'
 
 # Uncomment the below or paste into IRB
-account = Account.new
-account.execute_command(Deposit.new(20))
-account.execute_command(Deposit.new(40))
-account.execute_command(Deposit.new(20))
-account.execute_command(Withdraw.new(10))
-statement = Statement.new(account)
-statement.print_out
+# account = Account.new
+# account.execute_command(Deposit.new(20))
+# account.execute_command(Deposit.new(40))
+# account.execute_command(Deposit.new(20))
+# account.execute_command(Withdraw.new(10))
+# statement = Statement.new(account)
+# statement.print_out
